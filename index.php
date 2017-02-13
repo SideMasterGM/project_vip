d153835c1ab381992b3e754403d74d8c0f7fc9b8
Side Master <sidemasterdelfuturo@gmail.com>
2017-02-12 20:55:14 -0600

    Init index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <?php 
+        @session_start();
+        //session_destroy();
+        if (!isset($_SESSION['session']) || $_SESSION['session'] == "No"){
+            ?>
+                <head>
+                    <?php include ("source/php/head.php"); ?>
+                </head>
+
+                <body style="overflow: hidden;">
+                    <?php
+                        include ("source/php/content.php");
+                        include ("source/php/foot_js.php");
+                    ?>
+                </body>
+            <?php
+        } else {
+            //include ("private/desktop/design/codecanyon/index.php");
+            include ("private/desktop0/html/index.php");
+        }
+
+    ?>
+</html>
