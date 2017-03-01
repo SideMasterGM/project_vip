68995faafce7557a9998b24d7ba0fd2e5f591700
Side Master <sidemasterdelfuturo@gmail.com>
2017-03-01 08:22:57 -0600

    Compare users.php with html/users.php

--- private/desktop0/html/users.php
+++ private/desktop0/html/users.php
@@ -1,27 +1,20 @@
-<?php
-    @session_start();
-    if (@$_SESSION['login'] != 1){
-        header("Location: ../../");
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include ("build/head.php"); ?>
+        <?php include ("private/desktop0/html/build/head.php"); ?>
     </head>
 
-    <body class="flat-blue" style="background-image: url('../../images/back/bg1.jpg'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
+    <body class="flat-blue" style="background-image: url('source/img/back/bg1.jpg'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
         <div class="app-container">
             <div class="row content-container">
-                <?php include ("build/tape.php"); ?>
+                <?php include ("private/desktop0/html/build/tape.php"); ?>
                 
-                <?php include ("build/menu_left.php"); ?>
+                <?php include ("private/desktop0/html/build/menu_left.php"); ?>
                 
-                <?php include ("build/view_users.php"); ?>
+                <?php include ("private/desktop0/html/build/view_users.php"); ?>
             </div>
 
-            <?php include ("build/footer.php"); ?>
+            <?php include ("private/desktop0/html/build/footer.php"); ?>
         </div>
         <?php include ("build/scripts.php"); ?>
         <script type="text/javascript">
