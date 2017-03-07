95b9690c5a4266834f668f3cd7edcab6ebb0864b
Side Master <sidemasterdelfuturo@gmail.com>
2017-03-07 13:43:05 -0600

    Create the user

--- private/desktop0/html/build/CreateTheUser.php
+++ private/desktop0/html/build/CreateTheUser.php
@@ -1,5 +1,7 @@
 <?php
-	include ("../../connect_server/connect_server.php");
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("vip");
+	@session_start();
 
 	$username 		= $_POST['Enter_UserName'];
 	$email 			= $_POST['Enter_Email'];
@@ -12,44 +14,13 @@
 		echo "Rellene todos los campos!.";
 	} else {
 		if ($PassOne == $PassTwo){
-			$password = password_hash($PassOne, PASSWORD_DEFAULT);
-
-			$InsertUser = "INSERT INTO admin_info (username, date_log, date_log_unix, email) VALUES ('".$username."','".$date_log."','".$date_log_unix."','".$email."');";
-			$InsertLog	= "INSERT INTO user_admin (username, password) VALUES ('".$username."','".$password."');";
-
-			if ($Conexion->query($InsertUser)){
-				if ($Conexion->query($InsertLog)){
-
-					$path = "../../users/";
-					if (!file_exists($path))
-						@mkdir($path, 0777);
-
-					$path .= $username."/";
-					if (!file_exists($path))
-						@mkdir($path, 0777);
-
-					$path_publish = $path;
-					$path_agents = $path;
-
-					$path .= "img_perfil/";
-					if (!file_exists($path))
-						@mkdir($path, 0777);
-
-					$path_publish .= "publish_img/";
-					if (!file_exists($path_publish))
-						@mkdir($path_publish, 0777);
-
-					$path_agents .= "agents/";
-					if (!file_exists($path_agents))
-						@mkdir($path_agents, 0777);
-
-					echo "OK";
-				}
+			if ($CN->addNewUser($username, $PassTwo, $email, @$_SESSION['usr'])){
+				echo "OK";
+			} else {
+				echo "Fail";
 			}
-
-		} else {
+		} else{
 			echo "Verifique la contraseña.";
 		}
 	}
-
 ?>
