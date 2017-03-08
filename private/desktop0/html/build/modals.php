fca51d9cbbe1dcf0548cb7bdd23f6d7f71f7c6ae
Side Master <sidemasterdelfuturo@gmail.com>
2017-03-08 13:43:32 -0600

    Modals

--- private/desktop0/html/build/modals.php
+++ private/desktop0/html/build/modals.php
@@ -796,7 +796,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="MyCreateUserNow">Crear usuario</h4>
+                <h4 class="modal-title" id="MyCreateUserNow"><span class="icon fa fa-user"></span> Crear usuario</h4>
             </div>
             <div class="modal-body">
                 
