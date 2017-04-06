bcd61132e16b3833b7de21c1c7ed47f1572fc498
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-05 20:48:49 -0600

    Add Get Activity Message ME

---
+++ private/desktop0/html/build/getActivityMessageMe.php
@@ -0,0 +1,60 @@
+<?php                                
+    include ("../../../connect_server/connect_server.php");
+    include ("CalcDate.php");
+
+    $CN = CDB("vip");
+    @session_start();
+
+    if (is_array($CN->getActivityNotificationMessage(@$_SESSION['usr'], 10))){
+        foreach ($CN->getActivityNotificationMessage(@$_SESSION['usr'], 10) as $ActivityNotificationMessage) {
+               
+            if (is_array($CN->getActivity($ActivityNotificationMessage['id_activity'], 1))){
+                foreach ($CN->getActivity($ActivityNotificationMessage['id_activity'], 1) as $getActivity) {
+                    $QImg = $CN->getUserImgPerfil($getActivity['username'], "DESC", 1);
+                    $Path = "";
+
+                    if (is_array($QImg)){
+                        foreach ($QImg as $value) {
+                            $Path = "private/desktop0/".$value['folder'].$value['src'];
+                        }
+                    } else if (is_bool($QImg)) {
+                        $Path = "private/desktop0/img/img-default/bg_default.jpg";
+                    }
+
+                    ?>
+                        <a href="#" onclick="LoadMessage(<?php echo $getActivity['id_activity']; ?>);">
+                            <li>
+                                <img src="<?php echo $Path; ?>" width="60px" height="60px" class="profile-img pull-left">
+                           
+                                <div class="message-block">
+                                    <div><span class="username"><?php echo $getActivity['username']; ?></span> <span class="message-datetime"><?php echo nicetime(date("Y-m-d H:i", $getActivity['date_log_unix'])); ?></span>
+                                    </div>
+                                    <div class="message">
+                                        <?php 
+                                            echo substr($getActivity['description'], 0, 260); 
+
+                                            if (strlen($getActivity['description']) > 260){
+                                                echo "...";
+                                            }
+                                        ?>
+                                    </div>
+                                </div>
+
+                            </li>
+                        </a>
+                    <?php
+
+                }
+            } else if (is_bool($CN->getActivity($ActivityNotificationMessage['id_activity'], 1))){
+                echo "No hay actividad.";
+            }
+
+        }
+    } else if (is_bool($CN->getActivityNotificationMessage(@$_SESSION['usr'], 10))) {
+        echo "No hay notificaciones.";
+    }
+?>
+
+<form id="SendIdMessage">
+    <input type="hidden" id="IdMessage" name="IdMessage" value="" />
+</form>
