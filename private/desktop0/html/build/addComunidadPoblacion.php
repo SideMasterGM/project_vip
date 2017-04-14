5f7a40682c88c63cd90137ba226f64c7c3f44c14
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-14 15:39:35 -0600

    Nice.

--- private/desktop0/html/build/addComunidadPoblacion.php
+++ private/desktop0/html/build/addComunidadPoblacion.php
@@ -9,8 +9,8 @@
             foreach ($CN->getProjectComunidadPoblacion() as $value) {
                 ?>
                     <span class="label label-primary" style="font-size: 16px; background-color: #353D47; text-align: left; padding:10px; width:100%; margin: 10px 10px 0 0; display: inline-table;" ><i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                        <?php echo $value['nombrefac']; ?>
-                        <i class="fa fa-times" style="margin: 0 5px; position: absolute; right: 8%; cursor: pointer;" title="Eliminar <?php echo $value['nombrefac']; ?>" aria-hidden="true" onclick="javascript: DeleteTagComunidadPoblacion('<?php echo $value['codigo_facultad'] ?>');" ></i>
+                        <?php echo $value['nombre_muni']; ?>
+                        <i class="fa fa-times" style="margin: 0 5px; position: absolute; right: 8%; cursor: pointer;" title="Eliminar <?php echo $value['nombre_muni']; ?>" aria-hidden="true" onclick="javascript: DeleteTagComunidadPoblacion('<?php echo $value['cod_muni'] ?>');" ></i>
                     </span>
                 <?php
             }
