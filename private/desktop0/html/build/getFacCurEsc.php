17a9c61c3e5ee6eb8e5552493642b2b69f329d47
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-13 23:23:39 -0600

    Done

---
+++ private/desktop0/html/build/getFacCurEsc.php
@@ -0,0 +1,21 @@
+<?php
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("vip");
+
+	if (is_array($CN->getProjectFacCurEsc())){
+		?> <optgroup label="Lista de centros"> <?php
+
+		foreach ($CN->getProjectFacCurEsc() as $value) {
+			?>
+				<option value="<?php echo $value['codigo_facultad']; ?>"><?php echo $value['nombrefac']; ?></option>
+			<?php
+		}
+
+		?> </optgroup> <?php
+	} else if (is_bool($CN->getProjectFacCurEsc())){
+		?>
+	        <optgroup label="Lista de centros">
+	        </optgroup>
+	    <?php
+	}
+?>
