56a2da90d5d70cb31ba61f8bf180c17181eb6076
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-13 23:08:58 -0600

    Creating a new file script PHP

---
+++ private/desktop0/html/build/DelFacCurEsc.php
@@ -0,0 +1,20 @@
+<?php
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("vip");
+
+	$id = $_POST['DelTagInstanciaAprobacion'];
+
+	if ($CN->deleteInstanciaAprobacion($id)){
+		if (is_array($CN->getProjectInstanciaAprobacion())){
+            foreach ($CN->getProjectInstanciaAprobacion() as $value) {
+                ?>
+                    <span class="label label-primary" style="font-size: 16px; margin: 10px 10px 0 0; display: inline-table;" ><?php echo $value['nombre_instancia_aprobacion']; ?>
+                    	<i class="fa fa-times" style="margin: 0 5px; cursor: pointer;" title="Eliminar" aria-hidden="true" onclick="javascript: DeleteTagInstanciaAprobacion('<?php echo $value['id'] ?>');" ></i>
+                    </span>
+                <?php
+            }
+        } else if (is_bool($CN->getProjectInstanciaAprobacion())){
+            #Opcional para agregar un diálogo.
+        }
+	}
+?>
