8c1bb6762337877b9aef1b13281220c0595caadf
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-13 23:20:01 -0600

    Changes made.

--- private/desktop0/html/build/DelFacCurEsc.php
+++ private/desktop0/html/build/DelFacCurEsc.php
@@ -4,16 +4,16 @@
 
 	$id = $_POST['DelTagFacCurEsc'];
 
-	if ($CN->deleteInstanciaAprobacion($id)){
-		if (is_array($CN->getProjectInstanciaAprobacion())){
-            foreach ($CN->getProjectInstanciaAprobacion() as $value) {
+	if ($CN->deleteFacCurEsc($id)){
+		if (is_array($CN->getProjectFacCurEsc())){
+            foreach ($CN->getProjectFacCurEsc() as $value) {
                 ?>
-                    <span class="label label-primary" style="font-size: 16px; margin: 10px 10px 0 0; display: inline-table;" ><?php echo $value['nombre_instancia_aprobacion']; ?>
-                    	<i class="fa fa-times" style="margin: 0 5px; cursor: pointer;" title="Eliminar" aria-hidden="true" onclick="javascript: DeleteTagInstanciaAprobacion('<?php echo $value['id'] ?>');" ></i>
+                    <span class="label label-primary" style="font-size: 16px; margin: 10px 10px 0 0; display: inline-table;" ><?php echo $value['nombrefac']; ?>
+                    	<i class="fa fa-times" style="margin: 0 5px; cursor: pointer;" title="Eliminar <?php echo $value['nombrefac']; ?>" aria-hidden="true" onclick="javascript: DeleteTagFacCurEsc('<?php echo $value['codigo_facultad'] ?>');" ></i>
                     </span>
                 <?php
             }
-        } else if (is_bool($CN->getProjectInstanciaAprobacion())){
+        } else if (is_bool($CN->getProjectFacCurEsc())){
             #Opcional para agregar un diálogo.
         }
 	}
