672f9edfa6b1d60eb23da9f9ade36c70acd367a7
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-14 15:48:21 -0600

    getComunidadPoblacion ready.

--- private/desktop0/html/build/getComunidadPoblacion.php
+++ private/desktop0/html/build/getComunidadPoblacion.php
@@ -2,19 +2,19 @@
 	include ("../../../connect_server/connect_server.php");
 	$CN = CDB("all");
 
-	if (is_array($CN->getProjectFacCurEsc())){
-		?> <optgroup label="Lista de centros"> <?php
+	if (is_array($CN->getProjectComunidadPoblacion())){
+		?> <optgroup label="Lista de comunidades"> <?php
 
-		foreach ($CN->getProjectFacCurEsc() as $value) {
+		foreach ($CN->getProjectComunidadPoblacion() as $value) {
 			?>
-				<option value="<?php echo $value['codigo_facultad']; ?>"><?php echo $value['nombrefac']; ?></option>
+				<option value="<?php echo $value['cod_muni']; ?>"><?php echo $value['nombre_muni']; ?></option>
 			<?php
 		}
 
 		?> </optgroup> <?php
-	} else if (is_bool($CN->getProjectFacCurEsc())){
+	} else if (is_bool($CN->getProjectComunidadPoblacion())){
 		?>
-	        <optgroup label="Lista de centros">
+	        <optgroup label="Lista de comunidades">
 	        </optgroup>
 	    <?php
 	}
