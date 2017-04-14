131736a40d01f023bfe2b13801f98f2d66c80a84
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-13 23:27:32 -0600

    Modifying the value in the connection to the DB.

--- private/desktop0/html/build/getFacCurEsc.php
+++ private/desktop0/html/build/getFacCurEsc.php
@@ -1,6 +1,6 @@
 <?php
 	include ("../../../connect_server/connect_server.php");
-	$CN = CDB("vip");
+	$CN = CDB("all");
 
 	if (is_array($CN->getProjectFacCurEsc())){
 		?> <optgroup label="Lista de centros"> <?php
