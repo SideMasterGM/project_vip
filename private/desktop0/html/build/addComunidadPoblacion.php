c7737942deb33584497a013384d2626e58e00f29
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-14 13:38:16 -0600

    Creating new file script PHP that add a new community.

---
+++ private/desktop0/html/build/addComunidadPoblacion.php
@@ -0,0 +1,21 @@
+<?php
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("all");
+
+	$Enter = $_POST['writeFacCutEsc'];
+
+	if ($CN->addFacCurEsc($Enter)){
+        if (is_array($CN->getProjectFacCurEsc())){
+            foreach ($CN->getProjectFacCurEsc() as $value) {
+                ?>
+                    <span class="label label-primary" style="font-size: 16px; background-color: #353D47; text-align: left; padding:10px; width:100%; margin: 10px 10px 0 0; display: inline-table;" ><i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        <?php echo $value['nombrefac']; ?>
+                        <i class="fa fa-times" style="margin: 0 5px; position: absolute; right: 8%; cursor: pointer;" title="Eliminar <?php echo $value['nombrefac']; ?>" aria-hidden="true" onclick="javascript: DeleteTagFacCurEsc('<?php echo $value['codigo_facultad'] ?>');" ></i>
+                    </span>
+                <?php
+            }
+        } else if (is_bool($CN->getProjectFacCurEsc())){
+            #Opcional para agregar un diálogo.
+        }
+    }
+?>
