9862dfcd046bccf9896802695cd15e29b7a3a333
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-13 19:35:33 -0600

    Has been added a new file script that call a method PHP: addInstanciaAprobacion

--- private/desktop0/html/build/addInstanciaAprobacion.php
+++ private/desktop0/html/build/addInstanciaAprobacion.php
@@ -1,23 +1,21 @@
 <?php
 	
-	include ("../../connect_server/connect_server.php");
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("vip");
 
-	$Enter = $_POST['writeTagProperty_type'];
+	$Enter = $_POST['writeInstanciaAprobacion'];
 
-	if ($Conexion->query("INSERT INTO property_type (id, name_type) VALUES ('','".$Enter."');")){
-		$NewD = $Conexion->query("SELECT * FROM property_type;");
-		if ($NewD->num_rows > 0){
-			while ($R = $NewD->fetch_array(MYSQLI_ASSOC)){
-				?>
-					<span class="label label-primary" style="font-size: 16px; margin: 10px 10px 0 0; display: inline-table;" ><?php echo $R['name_type']; ?>
-
-						<i class="fa fa-times" style="margin: 0 5px; cursor: pointer;" title="Eliminar" aria-hidden="true" onclick="javascript: DeleteTagPropertyType('<?php echo $R['id'] ?>');" ></i>
-					</span>
-				<?php
-			}
-		}
+	if ($CN->addInstanciaAprobacion($Enter)){
+		if (is_array($CN->getProjectInstanciaAprobacion())){
+            foreach ($CN->getProjectInstanciaAprobacion() as $value) {
+                ?>
+                    <span class="label label-primary" style="font-size: 16px; margin: 10px 10px 0 0; display: inline-table;" ><?php echo $value['nombre_instancia_aprobacion']; ?>
+                    	<i class="fa fa-times" style="margin: 0 5px; cursor: pointer;" title="Eliminar" aria-hidden="true" onclick="javascript: DeleteTagInstanciaAprobacion('<?php echo $value['id'] ?>');" ></i>
+                    </span>
+                <?php
+            }
+        } else if (is_bool($CN->getProjectInstanciaAprobacion())){
+            #Opcional para agregar un diálogo.
+        }
 	}
-
-
-
 ?>
