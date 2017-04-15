28391c7c68fc1b473c4e03a36be49b6b5913e17f
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-15 00:40:11 -0600

    Delete code garbage

--- private/desktop0/html/build/AddArticle.php
+++ private/desktop0/html/build/AddArticle.php
@@ -76,32 +76,4 @@
 		echo "No se ha podido registrar el proyecto.";
 	}
 
-
-
-
-	
-	$Q = "INSERT INTO article (id_art, title, content_es, content_en, price, department, city, local_address, id_agent, business_type, property_type, property_state, bed_room, living_room, parking, kitchen, longitude, latitude, date_log, date_log_unix, username) VALUES ('','".$title."','".$content_es."','".$content_es."','".$price."','".$department."','".$city."','".$local_address."','".$agent."','".$business_type."','".$property_type."','".$property_state."','".$bed_room."','".$living_room."','".$parking."','".(int)$kitchen."','".$coord_longitude."','".$coord_latitude."',NOW(),'".$date_log_unix."','".$username."');";
-
-	if ($Conexion->query($Q)){
-		
-		$AskIDArticle = $Conexion->query("SELECT id_art FROM article ORDER BY id_art DESC LIMIT 1;")->fetch_array(MYSQLI_ASSOC);
-
-		$tmp_img = $Conexion->query("SELECT * FROM tmp_img;");
-
-		if ($tmp_img->num_rows > 0){
-			while ($GetTmpImg = $tmp_img->fetch_array(MYSQLI_ASSOC)){
-				$SetData = "INSERT INTO publish_img (id_img, folder, src, date_log, date_log_unix, id_art) VALUES ('','".$GetTmpImg['folder']."','".$GetTmpImg['src']."',NOW(),'".time()."','".$AskIDArticle['id_art']."');";
-				if (!$Conexion->query($SetData)){
-					echo "Ocurrió un error al intentar insertar: ".$GetTmpImg['src'].", en el artículo con el ID: ".$AskIDArticle['id_art'];
-				} else {
-					if (!$Conexion->query("DELETE FROM tmp_img WHERE id='".$GetTmpImg['id']."';")){
-						echo "Problema al intentar eliminar el registro con id: ".$GetTmpImg['id'].", de la tabla tmp_img.";
-					}
-				}
-			}
-		}
-	}
-
-	echo "OK";
-
 ?>
