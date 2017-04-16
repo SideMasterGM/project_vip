633d635b91f560fa610123df18d4be2a41396eba
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-15 21:10:28 -0600

    Writing code on vip_project_img. Deleting.

--- private/desktop0/html/build/DelImgProjectNow.php
+++ private/desktop0/html/build/DelImgProjectNow.php
@@ -1,8 +1,13 @@
 <?php
-	include ("../../connect_server/connect_server.php");
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("vip");
+
 	$id = $_POST['newidimgdel'];
 
 	$src = $_POST['MynImgDel'];
+
+	echo "El ID: ".$id." y el src: ".$src;
+	exit();
 
 	$result = $Conexion->query("SELECT * FROM publish_img WHERE src='".$src."';");
 	if ($result->num_rows > 0){
@@ -19,5 +24,24 @@
 	} else {
 		echo "No hay datos";
 	}
+?>
 
+<?php
+	include ("../../../connect_server/connect_server.php");
+	$CN = CDB("vip");
+	@session_start();
+
+	$src = trim(urldecode($_POST['MyNameImgDelete']));
+
+	if (is_array($CN->getTmpImgUnique(@$_SESSION['usr'], $src))){
+		foreach ($CN->getTmpImgUnique(@$_SESSION['usr'], $src) as $R) {
+			chmod("../../".$R['folder'].$R['src'], 0777);
+			unlink("../../".$R['folder'].$R['src']);
+		}
+
+		if ($CN->deleteTmpImg($src))
+			echo "OK";
+	} else if (is_bool($CN->getTmpImgUnique(@$_SESSION['usr'], $src))){
+		echo "Mandela";
+	}
 ?>
