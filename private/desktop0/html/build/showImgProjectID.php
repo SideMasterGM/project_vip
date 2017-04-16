0a848d775a37957cb7bf88ef8ca9200cc3bb9bde
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-15 19:18:35 -0600

    Creating functions in the last file scrpt PHP.

---
+++ private/desktop0/html/build/showImgProjectID.php
@@ -0,0 +1,17 @@
+<?php
+	include ("../../../connect_server/connect_server.php");
+
+    $CN = CDB("vip");
+
+    $id = $_POST['ValueArticleByID'];
+
+    if (is_array($CN->getProjectImgOnlyById($id))){
+    	foreach ($CN->getProjectImgOnlyById($id) as $value) {
+    		?>
+				<div class="container_imgnow">
+					<img onclick="javascript: SelectImgArticle(this);" src="<?php echo "private/desktop0/".$value['folder'].$value['src']; ?>" />
+				</div>
+			<?php
+    	}
+    }
+?>
