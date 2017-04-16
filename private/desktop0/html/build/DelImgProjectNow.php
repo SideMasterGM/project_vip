70114bfdf9a9c575e707981a3727d7411a439904
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-15 22:29:09 -0600

    file script finished.

--- private/desktop0/html/build/DelImgProjectNow.php
+++ private/desktop0/html/build/DelImgProjectNow.php
@@ -2,46 +2,23 @@
 	include ("../../../connect_server/connect_server.php");
 	$CN = CDB("vip");
 
-	$id = $_POST['newidimgdel'];
+	$id_project = $_POST['newidimgdel'];
+	$src 		= $_POST['MynImgDel'];
 
-	$src = $_POST['MynImgDel'];
+	if (is_array($CN->getProjectImg())){
+		foreach ($CN->getProjectImg() as $R) {
 
-	echo "El ID: ".$id." y el src: ".$src;
-	exit();
+			if ($R['id_project'] == $id_project && $R['src'] == $src){
 
-	$result = $Conexion->query("SELECT * FROM publish_img WHERE src='".$src."';");
-	if ($result->num_rows > 0){
-		$r = $result->fetch_array(MYSQLI_ASSOC);
-		
-		chmod("../../".$r['folder'].$src, 0777);
-		unlink("../../".$r['folder'].$src);
+				chmod("../../".$R['folder'].$R['src'], 0777);
+				unlink("../../".$R['folder'].$R['src']);
 
-		if ($Conexion->query("DELETE FROM publish_img WHERE src='".$src."';")){
-			echo "OK";
-		} else {
-			echo "Algo va mal";
+				if ($CN->deleteProjectImgBySrc($id_project, $src)){
+					echo "OK";
+				}
+			}
 		}
-	} else {
-		echo "No hay datos";
-	}
-?>
-
-<?php
-	include ("../../../connect_server/connect_server.php");
-	$CN = CDB("vip");
-	@session_start();
-
-	$src = trim(urldecode($_POST['MyNameImgDelete']));
-
-	if (is_array($CN->getTmpImgUnique(@$_SESSION['usr'], $src))){
-		foreach ($CN->getTmpImgUnique(@$_SESSION['usr'], $src) as $R) {
-			chmod("../../".$R['folder'].$R['src'], 0777);
-			unlink("../../".$R['folder'].$R['src']);
-		}
-
-		if ($CN->deleteTmpImg($src))
-			echo "OK";
-	} else if (is_bool($CN->getTmpImgUnique(@$_SESSION['usr'], $src))){
-		echo "Mandela";
+	} else if (is_bool($CN->getProjectImg())){
+		echo "Fail";
 	}
 ?>
