1b149078982bf22be653bee384e51048ff693292
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-15 18:45:02 -0600

    Finished. Deleting some lines.

--- private/desktop0/html/build/ShowDataProjectByID.php
+++ private/desktop0/html/build/ShowDataProjectByID.php
@@ -10,18 +10,4 @@
     		echo $value['contenido'];
     	}
     }
-
-    include ("../../connect_server/connect_server.php");
-    include ("CalcDate.php");
-
-
-    $Data = $Conexion->query("SELECT * FROM article WHERE id_art='".$id."';");
-    
-    if ($Data->num_rows == 1){
-      $Data = $Data->fetch_array(MYSQLI_ASSOC);
-
-      echo $Data['content_es'];
-
-    }
-
 ?>
