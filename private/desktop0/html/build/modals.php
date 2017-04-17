dfa5b4c187316b441fa6a9884fd3328a86f59cd4
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-17 10:09:11 -0600

    Change title and content to the modal window.

--- private/desktop0/html/build/modals.php
+++ private/desktop0/html/build/modals.php
@@ -679,10 +679,10 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="MyDelArtModal">Eliminar artículo</h4>
+                <h4 class="modal-title" id="MyDelArtModal">Eliminar proyecto</h4>
             </div>
             <div class="modal-body">
-                <p>¿Seguro que desea eliminar el Artículo?</p>
+                <p>¿Seguro que desea eliminar el Proyecto?</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
