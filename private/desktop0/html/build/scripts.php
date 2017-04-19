0d759f89375fa7d8b1bf0a4bf65c2a4b61014030
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-18 23:42:53 -0600

    Deleting structure of code on scripts.php

--- private/desktop0/html/build/scripts.php
+++ private/desktop0/html/build/scripts.php
@@ -15,30 +15,3 @@
 <script type="text/javascript" src="private/desktop0/js/modal.js"></script>
 <script type="text/javascript" src="private/desktop0/js/script.js"></script>
 <script type="text/javascript" src="private/desktop0/js/jquery-ui.js"></script>
-
-<!-- Definiendo los script CKEditor -->
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="https://alex-d.github.io/Trumbowyg/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/trumbowyg.min.js"></script>
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/plugins/base64/trumbowyg.base64.min.js"></script>
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/plugins/colors/trumbowyg.colors.min.js"></script>
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/plugins/noembed/trumbowyg.noembed.min.js"></script>
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/plugins/pasteimage/trumbowyg.pasteimage.min.js"></script>
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/plugins/preformatted/trumbowyg.preformatted.min.js"></script>
-    <script src="https://cdn.rawgit.com/Alex-D/Trumbowyg/v2.1.0/dist/plugins/upload/trumbowyg.upload.min.js"></script>
-
-    <!-- <script src="edit/ajax/libs/jquery/1/jquery.min.js"></script>
-    <script src="edit/dist/trumbowyg.min.js"></script>
-    <script src="edit/dist/plugins/base64/trumbowyg.base64.min.js"></script>
-    <script src="edit/dist/plugins/colors/trumbowyg.colors.min.js"></script>
-    <script src="edit/dist/plugins/noembed/trumbowyg.noembed.min.js"></script>
-    <script src="edit/dist/plugins/pasteimage/trumbowyg.pasteimage.min.js"></script>
-    <script src="edit/dist/plugins/preformatted/trumbowyg.preformatted.min.js"></script>
-    <script src="edit/dist/plugins/upload/trumbowyg.upload.min.js"></script>
-    <script src="edit/js/vendor/highlight.js"></script>
-    <script src="edit/js/main.js"></script> -->
-
-    <script src="private/desktop0/html/edit/js/vendor/highlight.js"></script>
-    <script src="private/desktop0/html/edit/js/main.js"></script>
