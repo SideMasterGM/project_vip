842a3d157dd1065af58ba88607b9b436a7d466f1
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-22 22:05:22 -0600

    testing a best translation

--- private/desktop0/html/build/addProjectResult.php
+++ private/desktop0/html/build/addProjectResult.php
@@ -2,14 +2,12 @@
 	$idp_result 	= $_POST['idp_result'];
 	$fpr_content 	= $_POST['fpr_content'];
 
-	if ($fpr_content == ""){
-		$fpr_content = "Nada que agregar";
+	if ($fpr_content != ""){
+		include ("../../../connect_server/connect_server.php");
+
+		$CN = CDB("vip");
+
+		if ($CN->addProjectResult($idp_result, $fpr_content))
+			echo "OK";
 	}
-
-	include ("../../../connect_server/connect_server.php");
-
-	$CN = CDB("vip");
-
-	if ($CN->addProjectResult($idp_result, $fpr_content))
-		echo "OK";
 ?>
