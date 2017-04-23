380a7efc436088e88379964db99424089f67eb9f
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-23 11:29:03 -0600

    Add new file script PHP.

---
+++ team.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <?php 
+        @session_start();
+        //session_destroy();
+        if (!isset($_SESSION['session']) || $_SESSION['session'] == "No"){
+            ?>
+                <head>
+                    <?php include ("source/php/head.php"); ?>
+                </head>
+
+                <body style="overflow: hidden;">
+                    <?php
+                        include ("source/php/content.php");
+                        include ("source/php/foot_js.php");
+                    ?>
+                </body>
+            <?php
+        } else {
+            include ("private/desktop0/html/team.php");
+        }
+
+    ?>
+</html>
