75415b1b217513c32ad5a16126a941fe80deb622
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-22 22:03:49 -0600

    Puff, to nathing.

--- private/desktop0/html/build/addProjectResult.php
+++ private/desktop0/html/build/addProjectResult.php
@@ -2,6 +2,10 @@
 	$idp_result 	= $_POST['idp_result'];
 	$fpr_content 	= $_POST['fpr_content'];
 
+	if ($fpr_content == ""){
+		$fpr_content = "Nada que agregar";
+	}
+
 	include ("../../../connect_server/connect_server.php");
 
 	$CN = CDB("vip");
