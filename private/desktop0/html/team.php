1dda272ac579a288b1405cb2982653403ed59eab
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-23 11:28:49 -0600

    add new file script

---
+++ private/desktop0/html/team.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include ("private/desktop0/html/build/head.php"); ?>
+    </head>
+
+    <body class="flat-blue" style="background-image: url('source/img/back/bg1.jpg'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;">
+        <div class="app-container">
+            <div class="row content-container">
+                <?php include ("private/desktop0/html/build/tape.php"); ?>
+                
+                <?php include ("private/desktop0/html/build/menu_left.php"); ?>
+                
+                <?php include ("private/desktop0/html/build/view_team.php"); ?>
+            </div>
+
+            <?php include ("private/desktop0/html/build/footer.php"); ?>
+        </div>
+        <?php include ("build/scripts.php"); ?>
+        <script type="text/javascript">
+            setTimeout(function(){
+                $("navbar-nav .active").removeClass("active");
+                $(".agents__item").addClass("active");
+            }, 100);
+        </script>
+    </body>
+</html>
