b7703f0865edf5c3704f885802dde8dc43d0e4f1
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-23 23:31:07 -0600

    create new file script php

---
+++ private/desktop0/html/build/addTeamProject.php
@@ -0,0 +1,13 @@
+<?php
+	$idp_result 	= $_POST['idp_result'];
+	$fpr_content 	= $_POST['fpr_content'];
+
+	if ($fpr_content != ""){
+		include ("../../../connect_server/connect_server.php");
+
+		$CN = CDB("vip");
+
+		if ($CN->addProjectResult($idp_result, $fpr_content))
+			echo "OK";
+	}
+?>
