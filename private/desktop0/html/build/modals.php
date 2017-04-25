6ae12901a36ca6b9f7751af395a16f5386b21bb5
Side Master <sidemasterdelfuturo@gmail.com>
2017-04-25 08:16:27 -0600

    Deleting lines

--- private/desktop0/html/build/modals.php
+++ private/desktop0/html/build/modals.php
@@ -46,72 +46,11 @@
                                     </div>
                                 </div>
 
-                                <div class="ContainerReturnTeamProject">
-                                    <style>
-                                        .PhotoTeamProject {
-                                          position: relative;
-                                          background: url('private/desktop0/img/img-default/team.jpg'); 
-                                          width: 100%; 
-                                          height:230px; 
-                                          background-size: cover; 
-                                          border: 3px solid lightgrey; 
-                                          float: left;
-                                        }
-
-                                        .PhotoTeamProject .camNewPhoto {
-                                          color: #fff;
-                                          font-size: 14px;
-                                          padding: 20px 20px 20px 20px;
-                                          visibility: hidden;
-                                        }
-
-                                        .PhotoTeamProject .camNewPhoto {
-                                          transition-duration: .1s;
-                                          transform: linear;
-                                          margin-top: -10px;
-                                        }
-
-                                        .PhotoTeamProject:hover > .camNewPhoto {
-                                          visibility: visible;
-                                          display: inline-block;
-
-                                          transition-duration: .1s;
-                                          transform: linear;
-                                          margin: 0px;
-                                          width: 100%;
-                                          background-color: rgba(0,0,0,0.5);
-                                        }
-
-                                        #FormImgTeamProjectUpdate #ChgImgTPUpdate {
-                                          visibility: hidden;
-                                        }
-                                    </style>
-
-                                     <div class="PhotoTeamProject">
-                                        <i class="fa fa-camera fa-2x icon-camera-change ChgAnimationPTP" onclick="javascript: ChgImgTeamProjectClick();" aria-hidden="true" title="Cambiar imagen del equipo"></i>
-                                        <div class="camNewPhoto"><p>Actualizar imagen</p>
-                                        </div>
-                                    </div>
+                                <div class="ShowInfoTeamProject">
+                                    
                                 </div>
 
-                                <form id="FormImgTeamProjectUpdate" enctype="multipart/form-data">
-                                    <input type="file" id="ChgImgTPUpdate" name="ChgImgTPUpdate" onchange="javascript: UploadImgTeamProject();" />
-                                </form>
 
-                                <form id="AssignSessionIDTeam">
-                                    <input type="hidden" id="IDInputIDTeam" name="IDInputIDTeam" />
-                                </form>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title showTitleTeamProject">Nombres y apellidos</h3>
-                                    </div>
-                                </div>
-
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title showTitleTeamProject">Nombres y apellidos</h3>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
