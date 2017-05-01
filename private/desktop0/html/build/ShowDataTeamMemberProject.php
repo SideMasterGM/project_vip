899679a3334f35354e6edfd24a02dd04cd9ae27f
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-01 14:31:25 -0600

    Change of position of the showing img.

--- private/desktop0/html/build/ShowDataTeamMemberProject.php
+++ private/desktop0/html/build/ShowDataTeamMemberProject.php
@@ -1,108 +1,118 @@
 <?php
-  include ("../../../connect_server/connect_server.php");
-  include ("CalcDate.php");
+    include ("../../../connect_server/connect_server.php");
+    include ("CalcDate.php");
 
-  $CN_VIP = CDB("vip");
-  @session_start();
+    $CN_VIP = CDB("vip");
+    @session_start();
 ?>
 
-<!-- Acá existe un problema, la imagen de perfil no se muestra -->
-
 <div class="ContainerReturnTeamMemberProject">
-  <style>
+    <style>
 
-    <?php
-        $id_team = @$_SESSION['id_team'];
+        <?php
+            $id_team = @$_SESSION['id_team'];
 
-        if (is_array($CN_VIP->getTeamMembers($id_team))){
-            
-            foreach ($CN_VIP->getTeamMembers($id_team) as $ValueTeamMembers) {
-                if ($ValueTeamMembers['firts_name'] == ""){
-                    $id_member  = $ValueTeamMembers['id_member'];
-                    $id_img     = $ValueTeamMembers['id_img'];
+            if (is_array($CN_VIP->getTeamMembers($id_team))){
+                
+                foreach ($CN_VIP->getTeamMembers($id_team) as $ValueTeamMembers) {
+                    if ($ValueTeamMembers['firts_name'] == ""){
+                        $id_member  = $ValueTeamMembers['id_member'];
+                        $id_img     = $ValueTeamMembers['id_img'];
 
-                    $QImg = $CN_VIP->getTeamMemberImgPerfilById($id_team, $id_img, "DESC", 1);
-                    
-                    if (is_array($QImg)){
-                        foreach ($QImg as $value) {
-                            $TeamDateLog        = $value['date_log'];
-                            $TeamDateLogUNIX    = $value['date_log_unix'];
-                          ?>
-                            .PhotoTeamMemberProject {
-                              position: relative;
-                              background: url('private/desktop0/<?php echo $value['folder'].$value['src']; ?>');
-                              width: 100%; 
-                              height:130px; 
-                              background-size: cover; 
-                              border: 3px solid lightgrey; 
-                              float: left;
+                        $QImg = $CN_VIP->getTeamMemberImgPerfilById($id_team, $id_img, "DESC", 1);
+                        
+                        if (is_array($QImg)){
+                            foreach ($QImg as $value) {
+                                $TeamDateLog        = $value['date_log'];
+                                $TeamDateLogUNIX    = $value['date_log_unix'];
+                              ?>
+                                .PhotoTeamMemberProject {
+                                    position: relative;
+                                    background: url('private/desktop0/<?php echo $value['folder'].$value['src']; ?>');
+                                    width: 100%; 
+                                    height:130px; 
+                                    background-size: cover; 
+                                    border: 3px solid lightgrey; 
+                                    float: left;
+                                }
+                              <?php
                             }
-                          <?php
-                        }
-                      } else if (is_bool($QImg)){
+                          } else if (is_bool($QImg)){
+                            ?>
+                                .PhotoTeamMemberProject {
+                                    position: relative;
+                                    background: url('private/desktop0/img/img-default/team.jpg'); 
+                                    width: 100%; 
+                                    height:130px; 
+                                    background-size: cover; 
+                                    border: 3px solid lightgrey; 
+                                    float: left;
+                                }
+                            <?php
+                          }
+                    } else {
                         ?>
                             .PhotoTeamMemberProject {
-                              position: relative;
-                              background: url('private/desktop0/img/img-default/team.jpg'); 
-                              width: 100%; 
-                              height:130px; 
-                              background-size: cover; 
-                              border: 3px solid lightgrey; 
-                              float: left;
+                                position: relative;
+                                background: url('private/desktop0/img/img-default/team.jpg'); 
+                                width: 100%; 
+                                height:130px; 
+                                background-size: cover; 
+                                border: 3px solid lightgrey; 
+                                float: left;
                             }
                         <?php
-                      }
+                    }
                 }
+            } else if (is_bool($CN_VIP->getTeamMembers($id_team))){
+                ?>
+                    .PhotoTeamMemberProject {
+                        position: relative;
+                        background: url('private/desktop0/img/img-default/team.jpg'); 
+                        width: 100%; 
+                        height:130px; 
+                        background-size: cover; 
+                        border: 3px solid lightgrey; 
+                        float: left;
+                    }
+                <?php
             }
-
-        } else if (is_bool($CN_VIP->getTeamMembers($id_team))){
-            ?>
-                .PhotoTeamMemberProject {
-                  position: relative;
-                  background: url('private/desktop0/img/img-default/team.jpg'); 
-                  width: 100%; 
-                  height:130px; 
-                  background-size: cover; 
-                  border: 3px solid lightgrey; 
-                  float: left;
-                }
-            <?php
-        }
-    ?>
+        ?>
   
-    .PhotoTeamMemberProject .camNewPhoto {
-      color: #fff;
-      font-size: 14px;
-      padding: 8px 20px 20px 20px;
-      visibility: hidden;
-    }
+        .PhotoTeamMemberProject .camNewPhoto {
+            color: #fff;
+            font-size: 14px;
+            padding: 8px 20px 20px 20px;
+            visibility: hidden;
+        }
 
-    .PhotoTeamMemberProject .camNewPhoto {
-      transition-duration: .1s;
-      transform: linear;
-      margin-top: -10px;
-    }
+        .PhotoTeamMemberProject .camNewPhoto {
+            transition-duration: .1s;
+            transform: linear;
+            margin-top: -10px;
+        }
 
-    .PhotoTeamMemberProject:hover > .camNewPhoto {
-      visibility: visible;
-      display: inline-block;
-      transition-duration: .1s;
-      transform: linear;
-      margin: 0px;
-      width: 100%;
-      background-color: rgba(0,0,0,0.5);
-    }
+        .PhotoTeamMemberProject:hover > .camNewPhoto {
+            visibility: visible;
+            display: inline-block;
+            transition-duration: .1s;
+            transform: linear;
+            margin: 0px;
+            width: 100%;
+            background-color: rgba(0,0,0,0.5);
+        }
 
-    #FormImgTeamMemberProjectUpdate #ChgImgTPMemberUpdate {
-      visibility: hidden;
-    }
-  </style>
+        #FormImgTeamMemberProjectUpdate #ChgImgTPMemberUpdate {
+            visibility: hidden;
+        }
+    </style>
 
-   <div class="PhotoTeamMemberProject">
-      <i class="fa fa-camera fa-2x icon-camera-change ChgAnimationPTP" onclick="javascript: ChgImgTeamMemberProjectClick();" aria-hidden="true" title="Cambiar imagen del integrante"></i>
-      <div class="camNewPhoto"><p>Actualizar imagen</p>
-      </div>
-  </div>
+    <div class="PhotoTeamMemberProject">
+        <i class="fa fa-camera fa-2x icon-camera-change ChgAnimationPTP" onclick="javascript: ChgImgTeamMemberProjectClick();" aria-hidden="true" title="Cambiar imagen del integrante"></i>
+        
+        <div class="camNewPhoto"><p>Actualizar imagen</p>
+        </div>
+    </div>
 </div>
 
 <form id="FormImgTeamMemberProjectUpdate" enctype="multipart/form-data">
@@ -153,24 +163,23 @@
         </div> 
 
 
-      <style>
-          div#collapseDesgracia input[type="button"] {
-            width: 100%;
-            padding: 4px;
-            border: none;
-            background-color: steelblue;
-            color: #fff;
-            margin-bottom: 2px;
-          }
+        <style>
+            div#collapseDesgracia input[type="button"] {
+                width: 100%;
+                padding: 4px;
+                border: none;
+                background-color: steelblue;
+                color: #fff;
+                margin-bottom: 2px;
+            }
 
-          div#collapseDesgracia input[type="button"]:hover {
-            background-color: teal;
-          }
+            div#collapseDesgracia input[type="button"]:hover {
+                background-color: teal;
+            }
 
-          div#collapseDesgracia input[type="button"]:focus {
-            background-color: rgba(0,0,0,0.4);
-          }
-      </style>               
-  </div>
-
+            div#collapseDesgracia input[type="button"]:focus {
+                background-color: rgba(0,0,0,0.4);
+            }
+        </style>               
+    </div>
 </div>
