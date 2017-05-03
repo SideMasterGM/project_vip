313cd1751f2b25fbaa11830edf0428e9783ea5eb
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-02 21:08:21 -0600

    Add new window modal

--- private/desktop0/html/build/modals.php
+++ private/desktop0/html/build/modals.php
@@ -60,9 +60,9 @@
                     <div class="modal-body">
                         <div class="row">
                             <div class="col-xs-12">
-                                <div class="panel panel-default">
+                                <div class="panel panel-default" style="padding-bottom: 15px;">
                                     <div class="panel-heading">
-                                        <h3 class="panel-title"><span class="icon fa fa-users"></span> Miembros del equipo <i class="fa fa-plus-circle buttons_addPanel" onclick="javascript: AddNewTeamMemberModal();" aria-hidden="true" title="Agregar nuevo integrante"></i></h3>
+                                        <h3 class="panel-title"><span class="icon fa fa-users"></span> Miembros del equipo <i class="fa fa-plus-circle buttons_addPanel" onclick="javascript: AreYouSureDeleteMember();" aria-hidden="true" title="Agregar nuevo integrante"></i></h3>
                                     </div>
 
                                     <div class="ShowInfoMembersTeamProject">
@@ -84,6 +84,28 @@
                 <button type="button" class="btn btn-danger" onclick="javascript: DelArtModal();">Eliminar</button>
                 <button type="button" class="btn btn-info" onclick="javascript: UpdateListItemArt();">Actualizar</button>
                 <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Mostrar una centinela, preguntando si realmente desea eliminar el miembro del equipo!. -->
+<input type="hidden" class="TeamProjectAreYouSureDeleting" data-toggle="modal" data-target="#TeamProjectAreYouSureDeleting"  />
+
+<!-- Modal -->
+<div class="modal fade modal-primary" id="TeamProjectAreYouSureDeleting" tabindex="1" role="dialog" aria-labelledby="myTeamProjectAreYouSureDeleting" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myTeamProjectAreYouSureDeleting">Eliminar miembro</h4>
+            </div>
+            <div class="modal-body">
+                <h4>Disculpe por desconfiar, ¿Está seguro que desea eliminar el miembro de este equipo?.</h4>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" onclick="javascript: window.location.reload();" data-dismiss="modal">¡Cancelar!...</button>
+                <button type="button" class="btn btn-danger" onclick="javascript: DelArtModal();">¡Eliminar!...</button>
             </div>
         </div>
     </div>
@@ -118,7 +140,7 @@
                             </div>
 
                             <div class="ShowInfoTeamMemberProjectAdd">
-
+                                
                             </div>
 
                         </div>
@@ -152,7 +174,6 @@
                                         </div>
 
                                         <form id="dataSendIDs">
-                                            
                                             <input type="hidden" id="dataSendIDs_firstname" name="dataSendIDs_firstname" value="" />
                                             <input type="hidden" id="dataSendIDs_lastname" name="dataSendIDs_lastname" value="" />
                                             
