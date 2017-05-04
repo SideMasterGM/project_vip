aed30b82117a9064a414dd26e328a9e0a62e9256
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-03 22:30:46 -0600

    Adding content to the file script addDelCoordinate

--- private/desktop0/html/build/addDelCoordinate.php
+++ private/desktop0/html/build/addDelCoordinate.php
@@ -1,3 +1,26 @@
 <?php
-	
+	include ("../../../connect_server/connect_server.php");
+
+	$CN_VIP = CDB("vip");
+	@session_start();
+
+	$id_team = $_SESSION['id_team'];
+
+	$id_member 	= $_POST['InputTextCoordinateIdMember'];
+	$CBValue 	= $_POST['InputTextMemberCBValue'];
+
+	if ($CBValue){
+
+		if ($CN_VIP->addCoordinator($id_member)){
+			echo "OK";
+		}
+
+	} else {
+
+		if ($CN_VIP->delCoordinator($id_member)){
+			echo "OK";
+		}
+
+	}
+
 ?>
