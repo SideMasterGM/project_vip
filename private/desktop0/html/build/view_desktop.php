c29e2335520573b50d1630e14616f72ac0fcf518
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-05 07:28:56 -0600

    Adding views of desktop mdoe

--- private/desktop0/html/build/view_desktop.php
+++ private/desktop0/html/build/view_desktop.php
@@ -1,8 +1,8 @@
 <div class="container-fluid">
     <div class="side-body padding-top">
         <div class="row">
-			<center><iframe src="https://bellyrealestate.com/new/" width="98%" height="530"> </iframe></center>
-			<?php include ("build/modals.php"); ?>
+			<center><iframe src="http://192.168.56.1/project_vip/" width="98%" height="530"> </iframe></center>
+			<?php include ("private/desktop0/html/build/modals.php"); ?>
         </div>
     </div>
 </div>
