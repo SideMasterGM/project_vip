f6a5d5f46d1606ffac607dc823f8f6bb45694dd5
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-05 07:29:06 -0600

    Adding view of mobile mode

--- private/desktop0/html/build/view_mobile.php
+++ private/desktop0/html/build/view_mobile.php
@@ -4,17 +4,17 @@
         	<div class="row no-margin-bottom">
             	<div class="col-sm-3 col-xs-12">
                 	<div class="card card-success">
-						<center><iframe src="https://bellyrealestate.com/new/" width="100%" height="530"> </iframe></center>
+						<center><iframe src="http://192.168.56.1/project_vip/" width="100%" height="530"> </iframe></center>
                 	</div>
                 </div>
 
                 <div class="col-sm-9 col-xs-12">
                 	<div class="card card-success">
-						<center><iframe src="https://bellyrealestate.com/new/" width="100%" height="530"> </iframe></center>
+						<center><iframe src="http://192.168.56.1/project_vip/" width="100%" height="530"> </iframe></center>
                 	</div>
                 </div>
             </div>
-            <?php include ("build/modals.php"); ?>
+            <?php include ("private/desktop0/html/build/modals.php"); ?>
         </div>
     </div>
 </div>
