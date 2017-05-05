703b1739a945349a08bc405bd9fc469b553152e8
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-05 00:42:25 -0600

    DelTeamComplete

--- private/desktop0/html/build/DelTeamComplete.php
+++ private/desktop0/html/build/DelTeamComplete.php
@@ -1,3 +1,12 @@
 <?php
-	
+	include ("../../../connect_server/connect_server.php");
+
+	$CN_VIP = CDB("vip");
+	@session_start();
+
+	$id_team = $_SESSION['id_team'];
+
+	if ($CN_VIP->DelTeamComplete($id_team)){
+		echo "OK";
+	}
 ?>
