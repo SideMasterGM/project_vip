305e7cca0cbc10f28c74289ce324877ccb94a25e
Side Master <sidemasterdelfuturo@gmail.com>
2017-05-27 01:42:45 -0600

    Making change

--- private/desktop0/html/build/view_report.php
+++ private/desktop0/html/build/view_report.php
@@ -5,8 +5,6 @@
         * --------------------------------------------- *
     */
 
-    include ("private/desktop0/html/build/modals.php");
-
     if (!isset($_POST['GenerateReportArticleID'])){
         ?>
             <div class="side-body padding-top" style="margin-left: 25%;">
@@ -41,17 +39,181 @@
                     <div class="row">
                         <div class="col-xs-12">
                             <div class="card">
-                                <div class="card-header">
-                                    <div class="card-title">
-                                        <div class="title">
-                                            <i class="fa fa-pencil-square-o"></i> <?php echo "Título del proyecto"; ?>
-                                        </div>
-                                    </div>
-                                    <button type="button" class="btn btn-primary" style="float: right; margin: 12px;" onclick="javascript: GenerateReport();" title="Generar un reporte completo del proyecto">Descargar en PDF</button>
-                                </div>
-                                <div class="card-body">
-                                    
-                                </div>
+                                
+                                <?php
+                                    $id = $_POST['GenerateReportArticleID'];
+                                    $CN_VIP = CDB("vip");
+                                    if (is_array($CN_VIP->getProjectsOnlyById($id))){
+                                        $ProjectInfo = $CN_VIP->getProjectsOnlyById($id);
+
+                                        foreach ($ProjectInfo as $value) { 
+                                            $ProjectNombre          = $value['nombre']; 
+                                            $ProjectIDFacCurEsc     = $value['id_facultad_cur_escuela']; 
+                                        }
+
+                                        $ProjectFacCurEsc = $CN_VIP->getOnlyFacCurEsc($id);
+
+                                        ?>
+                                            <div class="card-header">
+                                                <div class="card-title">
+                                                    <div class="title">
+                                                        <i class="fa fa-pencil-square-o"></i> <?php echo $ProjectNombre; ?>
+                                                    </div>
+                                                </div>
+                                                <button type="button" class="btn btn-primary" style="float: right; margin: 12px;" onclick="javascript: GenerateReport();" title="Generar un reporte completo del proyecto">Descargar en PDF</button>
+                                            </div>
+                                            <div class="card-body">
+                                                
+                                                <div class="row">
+                                                    <div class="col-xs-4">
+                                                        <div class="panel panel-default">
+                                                            <div class="panel-heading" style="background-color: #353D47; color: #fff;">
+                                                                <h3 class="panel-title">Identificación del proyecto
+                                                            </div>
+                                                            <div class="panel-body">
+                                                                <div>
+                                                                    <div class="row">
+                                                                        <div class="col-xs-8">
+                                                                            <p>
+                                                                                <b>Identificador</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-4">
+                                                                             <p>
+                                                                                <?php echo $id; ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="row">
+                                                                        <div class="col-xs-8">
+                                                                            <p>
+                                                                                <b>ID Facultad | CUR | Escuela</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-4">
+                                                                             <p>
+                                                                                <?php foreach ($ProjectInfo as $value) { echo $value['id_facultad_cur_escuela']; } ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="row">
+                                                                        <div class="col-xs-8">
+                                                                            <p>
+                                                                                <b>Fecha de aprobación</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-4">
+                                                                             <p>
+                                                                                <?php foreach ($ProjectInfo as $value) { echo $value['fecha_aprobacion']; } ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="row">
+                                                                        <div class="col-xs-8">
+                                                                            <p>
+                                                                                <b>Código de Dictámen Económico</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-4">
+                                                                             <p>
+                                                                                <?php foreach ($ProjectInfo as $value) { echo $value['cod_dictamen_economico']; } ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="row">
+                                                                        <div class="col-xs-8">
+                                                                            <p>
+                                                                                <b>ID de Instancia de Aprobación</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-4">
+                                                                             <p>
+                                                                                <?php foreach ($ProjectInfo as $value) { echo $value['id_instancia_aprobacion']; } ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-xs-8">
+                                                        <div class="panel panel-default">
+                                                            <div class="panel-heading" style="background-color: #353D47; color: #fff;">
+                                                                <h3 class="panel-title">Identificación del proyecto
+                                                            </div>
+                                                            <div class="panel-body">
+                                                                <div>
+                                                                    <div class="row">
+                                                                        <div class="col-xs-6">
+                                                                            <div class="panel">
+                                                                                <div class="panel-heading" role="tab" id="headingTeamMembers34" style="background-color: #5587CB;">
+                                                                                    <span class="panel-title">
+                                                                                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTeamMembers34" aria-expanded="false" aria-controls="collapseTeamMembers34" style="color: #fff;"><span class="icon fa fa-user"></span>
+                                                                                            Facultad | CUR | Escuela
+                                                                                        </a>
+                                                                                    </span>
+                                                                                </div>
+                                                                            </div>
+
+                                                                            <div id="collapseTeamMembers34" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTeamMembers34">
+                                                                                <div class="panel-body">
+                                                                                    <div class="row">
+                                                                                        <div class="col-xs-6">
+                                                                                            <?php 
+                                                                                                if (is_bool($ProjectFacCurEsc)){
+                                                                                                    echo "No hay";
+                                                                                                } else {
+                                                                                                     echo $ProjectFacCurEsc;
+                                                                                                }
+                                                                                                ?>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <p>
+                                                                                <b>Identificador</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-6">
+                                                                             <p>
+                                                                                <?php echo $id; ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="row">
+                                                                        <div class="col-xs-6">
+                                                                            <p>
+                                                                                <b>ID Facultad | CUR | Escuela</b>
+                                                                            </p>
+                                                                        </div>
+                                                                        <div class="col-xs-6">
+                                                                             <p>
+                                                                                <?php foreach ($ProjectInfo as $value) { echo $value['id_facultad_cur_escuela']; } ?>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        <?php
+
+                                    } else {
+                                        exit();
+                                    }
+                                ?>
+
                             </div>
                         </div>
                         
